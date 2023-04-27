2df266767cb559291d3d7339114bfbac779f61e4
ahulab <9096180+ahulab@users.noreply.github.com>
2023-04-27 03:56:57 +0000

    revisiting (#3)
    * Revisiting many years later
    
    * use requestanimationframe
    
    * remove dockerfile

---
+++ src/main.js
@@ -0,0 +1,106 @@
+var timer;
+var running = true;
+
+var slopeX = 2;
+var slopeY = 1;
+var x = 1;
+
+var $greeting = $('.greeting');
+var $bouncing_image = $('.bouncing_image');
+
+windowHeight = $(window).height() - $bouncing_image.width();
+windowWidth = $(window).width() - $bouncing_image.width();
+
+var startPos = function(){
+  // Get viewport dimensions (remove the dimension of the div)
+  var h = $(window).height() - $('.bouncing_image').width();
+  var startingHeight = Math.floor(Math.random() * h);
+  return startingHeight;    
+}
+
+var y = startPos();
+
+var startTimestamp, previousTimestamp;
+
+var reverse = function(direction) {
+  return direction *= -1;
+}
+
+// really ugly function for bouncing the image
+var bouncingBeauty = function(timestamp) {
+  if (startTimestamp === undefined) {
+    startTimestamp = timestamp;
+  }
+
+  var elapsed = timestamp - startTimestamp;
+
+  if (previousTimestamp !== timestamp) {
+    if (x > windowWidth) {
+      x = windowWidth;
+      slopeX = reverse(slopeX);
+    }
+    else if (x < 0) {
+      x = 0;
+      slopeX = reverse(slopeX);
+    } 
+    else if (y > windowHeight) {
+      y = windowHeight;
+      slopeY = reverse(slopeY);
+    }
+    else if (y < 0) {
+      y = 0;
+      slopeY = reverse(slopeY);
+    }
+
+    $bouncing_image.css({ 
+      left: (x+= slopeX)+'px', 
+      bottom: (y+= slopeY)+'px' 
+    });
+  }
+
+  if (running == true) {
+    previousTimeStamp = timestamp;
+    window.requestAnimationFrame(bouncingBeauty);
+  }
+
+}
+
+var start = function() {
+  window.requestAnimationFrame(bouncingBeauty);
+}
+
+$(document).ready(function(){
+  $bouncing_image.css('bottom', startPos() + 'px');
+  $bouncing_image.css('visibility', 'visible');
+
+  start();
+})
+
+$(window).on('resize', function() {
+  windowHeight = $(window).height() - $('.bouncing_image').width();
+  windowWidth = $(window).width() - $('.bouncing_image').width();
+})
+
+$bouncing_image.click(function() {
+  if (running) {
+    clearInterval(timer);
+
+    var greetingPos = $greeting.position();
+    var imgWidth = $bouncing_image.width();
+    $bouncing_image.animate({
+      left: $(window).width()/2 - imgWidth/2,
+      bottom: $(window).height()/2 - imgWidth/2
+    })
+
+  }
+  else {
+    var currentImgPos = $bouncing_image.position();
+    x = currentImgPos.left;
+    y = currentImgPos.top;
+    start();
+  }
+  running = !running;
+  
+  $greeting.toggleClass('-is--hidden'); 
+  $bouncing_image.toggleClass('rotate');
+});
